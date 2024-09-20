5ba35e01fc2d54d62527abe01e1c2d7aa26bd371
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-09-20 19:08:53 +0100

    updated broadcast script

--- script/NFTMarketplace.s.sol
+++ script/NFTMarketplace.s.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import {Script, console} from "forge-std/Script.sol";
-import {NFTMarketplace} from "../src/NFTMarketplace.sol";
+import { Script, console } from "forge-std/Script.sol";
+import { NFTMarketplace } from "../src/NFTMarketplace.sol";
 
 contract NFTMarketplaceScript is Script {
     NFTMarketplace public nftMarketplace;
 
-    function setUp() public {}
+    function setUp() public { }
 
     function run() public {
-        vm.startBroadcast();
-
+        vm.startBroadcast(vm.envUint("DO_NOT_LEAK"));
+        // Deploy the contract
         nftMarketplace = new NFTMarketplace();
-
+        // Stop broadcasting
         vm.stopBroadcast();
+
+        console.log("AreaCalculator deployed to:", address(nftMarketplace));
     }
 }
